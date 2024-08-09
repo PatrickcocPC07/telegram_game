c54f3d6935f5e6bcd7df349f17185eee2c2d0f35
Patrick Cheung <patrick.cheung@technine.io>
2024-08-09 15:36:48 +0800

    rename path

--- src/GameContainer.js
+++ src/GameContainer.js
@@ -5,7 +5,7 @@ const GameContainer = () => {
     <>
       <div style={{ width: "100%", height: "100vh" }}>
         <iframe
-          src={"/WebGl/index.html"}
+          src={"telegram_game/WebGl/index.html"}
           width="100%"
           height="100%"
           style={{ border: "none" }}
