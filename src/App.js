09cdabca622385e6d7b1180dcd73593a61977aaa
Patrick Cheung <patrick.cheung@technine.io>
2024-08-09 16:06:37 +0800

    router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import GameContainer from "./GameContainer";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <GameContainer/>
+      <Routes>
+        <Route path="/telegram_game" element={<GameContainer />} />
+      </Routes>
     </div>
   );
 }
