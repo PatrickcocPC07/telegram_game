713ad09ddf5da73939f24514027bb6df868b2436
Patrick Cheung <patrick.cheung@technine.io>
2024-08-09 13:05:58 +0800

    init  code

---
+++ telegram-game/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import GameContainer from "./GameContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <GameContainer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ telegram-game/src/GameContainer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const GameContainer = () => {
+  return (
+    <>
+      game
+      <div style={{ width: "100%", height: "100vh" }}>
+        <iframe
+          src={"/WebGl/index.html"}
+          width="100%"
+          height="100%"
+          style={{ border: "none" }}
+          title="HTML5 Game"></iframe>
+      </div>
+    </>
+  );
+};
+
+export default GameContainer;
